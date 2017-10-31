77c17257f8df823aee2eecab1e157e133f133daf
ogonza46 <oliverags3@gmail.com>
2017-10-31 20:31:20 +0100

    Basement structure for exercise 6

---
+++ Exercici6/src/Exercici6/Exercici6.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercici6;
+
+import java.util.*;
+
+/**
+ * Exercici 6 exercicis colaboratius.
+ * @author Oliver Josep
+ */
+public class Exercici6 {
+    public static void main(String[] args) {
+        Exercici6 myApp =  new Exercici6();
+        myApp.run();
+    }
+    
+    public void run(){
+        //TODO
+        /*
+        menu(); 
+        reverse(); 
+        countMax();
+        countMin();
+        countAll();
+        exit;
+        */
+    }
+    
+    public void menu(){
+        
+    }
+    /**
+     * 
+     * @param seq
+     * @return 
+     */
+    public String revSeq(String seq){
+        //TODO
+        return "ATTACA";
+    }
+    
+    /**
+     * 
+     * @param seq
+     * @return 
+     */
+    public int countMax(String seq){
+        
+        return 100;
+    }
+    
+    /**
+     * 
+     * @param seq
+     * @return 
+     */
+    public int countMin(String seq){
+        
+        return 0;
+    }
+    
+    /**
+     * 
+     * @param seq
+     * @return 
+     */
+    public Map countAll(String seq){
+        Map <String, Integer> result;
+        result = new HashMap<String, Integer>();
+              
+        return null;
+    }
+    
+    public Scanner inputKeyb(){
+        Scanner scan = new Scanner(System.in);
+        return scan;
+    }
+    
+    
+}
